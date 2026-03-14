bc6296d2facbafb0b5e0cc952af0e1e72e0857ae
LginvM <84954335+LginvM@users.noreply.github.com>
2026-03-14 20:52:24 +0600

    ViewModel itemlist

---
+++ app/src/main/java/ru/loginov/room/activitys/detail/DetailViewModel.kt
@@ -0,0 +1,173 @@
+package ru.loginov.room.activitys.detail
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.google.android.engage.shopping.datamodel.ShoppingList
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import ru.loginov.room.DAO.models.Item
+import ru.loginov.room.DAO.models.Store
+import ru.loginov.room.DAO.models.shoppingList
+import ru.loginov.room.ui.Category
+import ru.loginov.room.ui.Graph
+import ru.loginov.room.ui.Utils
+import ru.loginov.room.ui.repository.Repository
+import java.util.Date
+
+class DetailViewModel
+    constructor(
+        private val itemId:Int,
+        private val repository: Repository = Graph.repository,
+    )
+
+    : ViewModel() {
+
+    var state by mutableStateOf(DetailState())
+        private set
+
+    init{
+            addListItem()
+            getStores()
+            if (itemId!=-1){
+                viewModelScope.launch {
+                    repository.getItemWithStoreAndList(itemId)
+                        .collectLatest {
+                            state = state.copy(
+                                item = it.item.itemName,
+                                store = it.store.storeName,
+                                date = it.item.date,
+                                category = Utils.category.find {
+                                    c ->  c.id == it.shoppingList.id } ?: Category(),
+                                qty = it.item.qty
+                            )
+                        }
+                }
+            }
+        }
+
+    init {
+            state= if(itemId!=-1){
+                state.copy(isUpdatingItem = true)
+            }else(
+                state.copy(isUpdatingItem = false)
+            )
+        }
+
+        val isFieldsNotEmpty:Boolean
+            get() = state.item.isNotEmpty() &&
+                    state.store.isNotEmpty() &&
+                    state.qty.isNotEmpty()
+
+    fun onItemChange(newValue:String){
+        state = state.copy(item = newValue)
+    }
+
+    fun onStoreChange(newValue:String){
+        state = state.copy(store = newValue)
+    }
+
+    fun onQtyChange(newValue:String){
+        state = state.copy(qty = newValue)
+    }
+
+    fun onDateChange(newValue:Date){
+        state = state.copy(date = newValue)
+    }
+
+    fun onCategoryChange(newValue:Category){
+        state = state.copy(category = newValue)
+    }
+
+    fun onScreenDialogDismissed(newValue: Boolean){
+        state = state.copy(isScreenDialogDismissed = newValue)
+    }
+
+    private fun addListItem(){
+        viewModelScope.launch {
+            Utils.category.forEach{
+            repository.insertList(
+                shoppingList(
+                    id = it.id,
+                    name = it.title
+                )
+            )
+            }
+        }
+    }
+
+    fun addShoppingItem(){
+        viewModelScope.launch {
+            repository.insertItem(
+                Item(
+                    itemName = state.item,
+                    listId = state.category.id,
+                    date= state.date,
+                    qty = state.qty,
+                    storeIdFk = state.StoreList.find{
+                        it.storeName == state.store
+                    }?.id ?: 0,
+                    isChecked = false
+                )
+            )
+        }
+    }
+
+    fun updateShoppingItem(id:Int){
+        viewModelScope.launch {
+            repository.insertItem(
+                Item(
+                    itemName = state.item,
+                    listId = state.category.id,
+                    date= state.date,
+                    qty = state.qty,
+                    storeIdFk = state.StoreList.find{
+                        it.storeName == state.store
+                    }?.id ?: 0,
+                    isChecked = false,
+                    id = id
+                )
+            )
+        }
+    }
+
+    fun addStore(){
+        viewModelScope.launch {
+            repository.insertStore(
+                Store(
+                    storeName = state.store,
+                    listIdFk = state.category.id
+                )
+            )
+        }
+    }
+
+    fun getStores(){
+        viewModelScope.launch {
+            repository.store.collectLatest {
+                state = state.copy(it)
+            }
+        }
+    }
+}
+
+class DetailViewModelFactor(private val id:Int): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return DetailViewModel(itemId = id) as T
+    }
+}
+
+
+data class DetailState(
+    val StoreList:List<Store> = emptyList(),
+    val item:String = "",
+    val store:String = "",
+    val date: Date = Date(),
+    val qty:String = "",
+    val isScreenDialogDismissed: Boolean = true,
+    val isUpdatingItem: Boolean = false,
+    val category: Category = Category(),
+)
--- app/src/main/java/ru/loginov/room/ui/Category.kt
+++ app/src/main/java/ru/loginov/room/ui/Category.kt
@@ -1,5 +1,21 @@
 package ru.loginov.room.ui
 
-class Category() {
-    val id = 0
+import androidx.annotation.DrawableRes
+import ru.loginov.room.R
+
+object Utils{
+    val category = listOf(
+        Category(title= "Drinks",resId = R.drawable.ic_launcher_background,id = 0),
+        Category(title= "Vegetable",resId = R.drawable.ic_launcher_background,id = 1),
+        Category(title= "Fruits",resId = R.drawable.ic_launcher_background,id = 2),
+        Category(title= "Cleaning",resId = R.drawable.ic_launcher_background,id = 3),
+        Category(title= "Electronic",resId = R.drawable.ic_launcher_background,id = 4) ,
+        Category(title= "None",resId = R.drawable.ic_launcher_background,id = 5)
+    )
 }
+
+data class Category(
+    @DrawableRes val resId: Int = -1,
+    val title:String = "",
+    val id: Int = -1,
+)
